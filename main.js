741248845447bfc1c02102908c6c2769fc77cc28
OscarKarimov <99991796+OscarKarimov@users.noreply.github.com>
2022-03-09 21:44:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function timeShover() {
+    var timeNow = new Date ();
+
+document.querySelector(".hours").textContent = timeNow.getHours();
+document.querySelector(".minutes").textContent = timeNow.getMinutes();
+document.querySelector(".seconds").textContent = timeNow.getSeconds();
+}
+timeShover();
+
+setInterval (function(){
+timeShover();
+},1000) 
